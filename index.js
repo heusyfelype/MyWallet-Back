612cac65e2d2fb86c4df7174bdc6cc20d68af64b
heusyfelype <felype.heusy@gmail.com>
2022-05-07 16:36:24 -0300

    feat: adicionada a função sign-in e a ecriptação de senha

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import cors from 'cors';
 import joi from "joi";
 import dotenv from 'dotenv'
 import { MongoClient, ObjectId } from "mongodb";
-
+import { v4 as uuid } from 'uuid';
+import bcrypt from 'bcrypt';
 
 //import { signUp, signIn } from './controllers/authController.js';
 
+//const token = uuid();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -47,6 +49,7 @@ app.post('/sign-up', async (req, res) =>{
         }
 
         delete signUp.confirm_password
+        signUp.password = bcrypt.hashSync(signUp.password, 10);
         const insert = await dataBase.collection("users").insertOne(signUp)
         
         res.status(201).send("Usuário adicionado com sucesso");
@@ -58,6 +61,42 @@ app.post('/sign-up', async (req, res) =>{
         return;
     }
 });
+
+
+app.post('/sign-in', async (req, res) =>{
+    const signIn = req.body;
+
+    const signInSchema = joi.object({
+        email: joi.string().email(),
+        password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{4,15}$')).required(),
+    });
+
+    const validationSignIn = signInSchema.validate(signIn, { abortEarly: false });
+    if (validationSignIn.error) {
+        res.status(422).send("Foi digitada alguma informação incorretamente", validationSignIn.error.details.message)
+        return;
+    }
+
+    try{
+        const user = await dataBase.collection("users").findOne({email: signIn.email})
+        if(!user || !bcrypt.compareSync(signIn.password, user.password)){
+            return res.status(422).send("email ou senha errado")
+        }
+        //const userId = user._id;
+        // const tokentest = uuid()
+        const onlineUser = {
+            name: user.name,
+            userId: user._id,
+            token: uuid()
+        }
+
+        await dataBase.collection("sessions").insertOne({...onlineUser, status: Date.now()})
+
+        return res.status(200).send(onlineUser)
+    }catch(e){
+        return res.send(e)
+    }
+})
 
 
 app.listen(5000, () => {
